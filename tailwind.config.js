fdd2a7689710d248bd17bebb900f04f12a92b5db
Tejas Keshwani <evadshell69@gmail.com>
2024-04-05 23:40:06 +0530

    tailwindsetup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        dark: '#2c3e50', // Dark Bluish Color
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
