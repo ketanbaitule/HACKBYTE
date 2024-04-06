704b645b745a829e402a6cf9c646a55ef6d74511
Tejas Keshwani <evadshell69@gmail.com>
2024-04-07 00:03:59 +0530

    upload docs added

--- src/app/dashboard/incident/[id]/AddModal.jsx
+++ src/app/dashboard/incident/[id]/AddModal.jsx
@@ -1,44 +1,90 @@
-"use client"
-import React, {useState} from 'react';
-import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, FormControl, FormLabel, Textarea } from "@chakra-ui/react"
+"use client";
+import React, { useState } from "react";
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  FormControl,
+  FormLabel,
+  Textarea,
+} from "@chakra-ui/react";
+import { Select } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
+function AddModal({
+  id,
+  modalDisclosure,
+  action,
+  title,
+  textAreaLabel,
+  textAreaDescription,
+}) {
+  const { isOpen, onOpen, onClose } = modalDisclosure;
+  const [loading, setLoading] = useState(false);
+  const [textArea, setTextArea] = useState("");
+  const [value, setValue] = useState("markdown");
 
-function AddModal({id, modalDisclosure, action, title, textAreaLabel, textAreaDescription}) {
-    const { isOpen, onOpen, onClose } = modalDisclosure;
-    const [loading, setLoading] = useState(false);
-    const [textArea, setTextArea] = useState("");
+  const submitForm = async () => {
+    setLoading(true);
+    await action(id, textArea);
+    onClose();
+    setLoading(false);
+    setTextArea("");
+  };
 
-    const submitForm = async ()=>{
-        setLoading(true);
-        await action(id, textArea);
-        onClose();
-        setLoading(false);
-        setTextArea("")
-    }
 
-    return (
-        <>
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>{title}</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <FormControl>
-                            <FormLabel>{textAreaLabel}</FormLabel>
-                            <Textarea value={textArea} onChange={(e)=>setTextArea(e.target.value)} placeholder={textAreaDescription} />
-                        </FormControl>
-                    </ModalBody>
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Select value={value} onChange={(e)=>{
+                setValue(e.target.value)
+            }} >
+              <option  value="markdown">markdown</option>
+              <option value="document">document</option>
+            </Select>
+            {value === "markdown" ? (
+              <FormControl>
+                <FormLabel>{textAreaLabel}</FormLabel>
+                <Textarea
+                  value={textArea}
+                  onChange={(e) => setTextArea(e.target.value)}
+                  placeholder={textAreaDescription}
+                />
+              </FormControl>
+            ) : (
+              <FormControl>
+                <Input placeholder="upload files" size="sl" type="file" />
+              </FormControl>
+            )}
+          </ModalBody>
 
-                    <ModalFooter>
-                        <Button isLoading={loading} colorScheme="blue" mr={3} onClick={submitForm}>
-                            Submit
-                        </Button>
-                        <Button variant="ghost" onClick={onClose}>Cancel</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </>
-    )
+          <ModalFooter>
+            <Button
+              isLoading={loading}
+              colorScheme="blue"
+              mr={3}
+              onClick={submitForm}
+            >
+              Submit
+            </Button>
+            <Button variant="ghost" onClick={onClose}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
 
 export default AddModal;
